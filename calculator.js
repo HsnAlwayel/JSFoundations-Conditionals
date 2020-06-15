f295ebb80422fbb70299e5b53650e407d70d7245
Hassan Alwayel <hsnalwayel@gmail.com>
2020-06-15 19:46:01 +0300

    Day2Task2

--- calculator.js
+++ calculator.js
@@ -1,3 +1,8 @@
 // Define the numbers and the operation here
-
+const var1 = 3;
+const var2 = 5;
+let addition = 8;
 // Write if-statements here
+if (var1 + var2 === addition) {
+  console.log(`${var1}+${var2}=${addition}`);
+}
